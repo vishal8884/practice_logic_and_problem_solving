6d7351a5ab00b3227ccc07cda17dba2ed53300f1
vishal <vishalambekar459@gmail.com>
2022-07-29 02:08:48 +0530

    floor and target code added

---
+++ src/Course/binarySearch/problems/NearestNuberToTarget_Ceiling_Q1.java
@@ -0,0 +1,38 @@
+package Course.binarySearch.problems;
+
+public class NearestNuberToTarget_Ceiling_Q1 {
+
+	public static void main(String[] args) {
+		int[] arr = {2,3,8,13,60,77,83};
+		
+		int res = nearestGreaterBinarySearch_Ceiling(arr, 65);   //65
+		System.out.println("res :: "+res);
+	}
+	//question --> find the target or number greater than target by min
+	
+	public static int nearestGreaterBinarySearch_Ceiling(int[] arr, int target) {
+		
+		if(target > arr[arr.length-1]) {
+			return -1;
+		 }
+		
+		   int start = 0;
+		   int end = arr.length-1;
+		   
+		   while(start <= end) {
+			   int mid = start + (end - start)/2;
+			   System.out.println("start :: "+start+"     end :: "+end+"      mid :: "+mid+"     mid val :: "+arr[mid]);
+			   
+			   if(target == arr[mid])
+				   return mid;
+			   if(target < arr[mid]) {
+				   end = mid -1;
+			   }
+			   else if(target > arr[mid]) {
+				   start = mid +1;
+			   }
+			   
+		   }
+		   return start;
+	}
+}
---
+++ src/Course/binarySearch/problems/NearestNuberToTarget_Floor_Q2.java
@@ -0,0 +1,37 @@
+package Course.binarySearch.problems;
+
+public class NearestNuberToTarget_Floor_Q2 {
+
+	public static void main(String[] args) {
+		int[] arr = {2,3,8,13,60,77,83};
+		
+		int res = nearestGreaterBinarySearch_Floor(arr, -5);   //65
+		System.out.println("res :: "+res);
+	}
+	
+	//question --> find the target or number less than target by min
+	
+	public static int nearestGreaterBinarySearch_Floor(int[] arr, int target) {
+		
+
+		   int start = 0;
+		   int end = arr.length-1;
+		   
+		   while(start <= end) {
+			   int mid = start + (end - start)/2;
+			   System.out.println("start :: "+start+"     end :: "+end+"      mid :: "+mid+"     mid val :: "+arr[mid]);
+			   
+			   if(target == arr[mid])
+				   return mid;
+			   if(target < arr[mid]) {
+				   end = mid -1;
+			   }
+			   else if(target > arr[mid]) {
+				   start = mid +1;
+			   }
+			   
+		   }
+		   
+		   return end;
+	}
+}
