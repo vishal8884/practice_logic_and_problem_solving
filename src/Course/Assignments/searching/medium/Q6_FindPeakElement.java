186147461c878eb7db345977689a078b69a2544b
vishal <vishalambekar459@gmail.com>
2022-08-14 23:37:16 +0530

    medium 14 and 15 added (rotated array)

---
+++ src/Course/Assignments/searching/medium/Q6_FindPeakElement.java
@@ -0,0 +1,15 @@
+package Course.Assignments.searching.medium;
+
+public class Q6_FindPeakElement {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int findPeakElement(int[] nums) {
+
+		
+		return -1;
+	}
+
+}
