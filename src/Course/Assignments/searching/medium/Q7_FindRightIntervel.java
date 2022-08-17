019213b47849aa536e2c28a3e21405604b64ded7
vishal <vishalambekar459@gmail.com>
2022-08-17 20:21:20 +0530

    Q7 find right intervel linear approach added

--- src/Course/Assignments/searching/medium/Q7_FindRightIntervel.java
+++ src/Course/Assignments/searching/medium/Q7_FindRightIntervel.java
@@ -1,6 +1,10 @@
 package Course.Assignments.searching.medium;
 
+
+
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Q7_FindRightIntervel {
 
@@ -10,13 +14,49 @@ public class Q7_FindRightIntervel {
 		int[][] arr2 = {{1,2}};
 		int[][] arr3 =  {{1,4},{2,3},{3,4}};
 		int[][] arr4 = {{1,2},{2,3},{0,1},{3,4}};     //expected res = 1,3,0,-1
-		int[] res = findRightInterval(arr4);
+		int[] res = findRightInterval(arr);
 		
 		System.out.println("res :: "+Arrays.toString(res));
 	}
 
 	public static int[] findRightInterval(int[][] arr){
 		if(arr.length <=1 ) return new int[]{-1};
+		
+		int[] res = new int[arr.length];
+		Arrays.fill(res, -1);
+		int firstFoundi = Integer.MAX_VALUE;
+
+		for(int i=0;i<arr.length;i++) {
+	        for(int j =0;j<arr.length;j++) {
+	        	System.out.println("arr[i][1] :: "+arr[i][1]+"      arr[j][0] :: "+arr[j][0]+"       i :: "+i+"       j :: "+j);
+	        	
+	        	if(arr[i][1] <= arr[j][0] && arr[j][0] <= firstFoundi) {
+	        		res[i] = j;
+	        		firstFoundi = arr[j][0];
+	        		System.out.println("---");
+	        		continue;
+	        	}
+	        }
+	        firstFoundi = Integer.MAX_VALUE;
+	        System.out.println();
+		}
+		
+		return res;
+	}
+
+}
+
+
+
+
+
+
+/*
+
+public static int[] findRightInterval(int[][] arr){
+		if(arr.length <=1 ) return new int[]{-1};
+		
+		Map<Integer, Integer> map = new HashMap<>();
 		
 		int[] res = new int[arr.length];
 		Arrays.fill(res, -1);
@@ -36,4 +76,4 @@ public class Q7_FindRightIntervel {
 		return res;
 	}
 
-}
+*/
