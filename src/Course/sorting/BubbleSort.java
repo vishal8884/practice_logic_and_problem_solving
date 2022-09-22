ff20458c81b994e39a4cd8bd35a4089ed5e7d584
vishal <vishalambekar459@gmail.com>
2022-09-23 01:08:35 +0530

    bubble sort and selection sort added

--- src/Course/sorting/BubbleSort.java
+++ src/Course/sorting/BubbleSort.java
@@ -16,21 +16,20 @@ public class BubbleSort {
 	
 	
     public static void bubbleSort(int[] arr) {
-    	
-    	boolean swapped;
+    	boolean isSwapped;
     	
     	for(int i=0;i<arr.length;i++) {
-    		swapped = false;
+    		isSwapped = false;
     		for(int j=1;j<arr.length-i;j++) {
     			if(arr[j-1] > arr[j]) {
-    				int temp = arr[j];
-    				arr[j] = arr[j-1];
-    				arr[j-1] = temp;
-    				swapped = true;
+    				int temp = arr[j-1];
+    				arr[j-1] = arr[j];
+    				arr[j] = temp;
+    				isSwapped = true;
     			}
     		}
     		
-    		if(!swapped) {
+    		if(!isSwapped) {
     			return;
     		}
     	}
