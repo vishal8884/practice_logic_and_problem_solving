b7f2fbb73eaf36b3e746df0eecf035c93bc630dd
vishal <vishalambekar459@gmail.com>
2022-10-09 22:48:22 +0530

    find the duplicate leetcode sol added

---
+++ src/Course/sorting/leetcode/lecture/Q3_FindTheDuplicateNumber.java
@@ -0,0 +1,36 @@
+package Course.sorting.leetcode.lecture;
+
+import java.util.Arrays;
+
+public class Q3_FindTheDuplicateNumber {
+
+	public static void main(String[] args) {
+
+		int[] arr = {1,2,3,4,3,2};
+		findDuplicate(arr);
+		
+		System.out.println("arr after :: "+Arrays.toString(arr));
+	}
+
+	public static int findDuplicate(int[] nums) {
+		int i=0;
+		while(i < nums.length) {
+			int correct = nums[i] - 1;
+			if(nums[i] != nums[correct]) {
+				swap(nums, i, correct);
+			}
+			else {
+				i++;
+			}
+		}
+		System.out.println("nums :: "+Arrays.toString(nums));
+		return nums[nums.length-1];
+	}
+	
+	public static void swap(int[] arr,int a,int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+
+}
